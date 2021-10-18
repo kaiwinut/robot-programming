607de66258c5b08b79087569ba98f8d46d35b96b
Kai Wang <kaiw018@gcc.u-tokyo.ac.jp>
2021-10-19 06:39:46 +0900

    Some bug fixes and adjustments to make movement smoother

--- 1011/track_box_to_cmd_vel.py
+++ 1011/track_box_to_cmd_vel.py
@@ -16,7 +16,7 @@ class track_box_to_cmd_vel:
 		rospy.init_node('client')
 
 		rospy.Subscriber('/camshift/track_box', RotatedRectStamped, self.call_back)
-		self.pub = rospy.Publisher('/cmd_vel', Twist)
+		self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
 
 		rospy.Timer(rospy.Duration(0.1), self.loop_once)
 
@@ -29,25 +29,29 @@ class track_box_to_cmd_vel:
 	def loop(self):
 		rate = rospy.Rate(10)
 		while not rospy.is_shutdown():
-			self.loopOnce()
+			self.loop_once()
 			rate.sleep()
 
-	def loop_once(self, event):
+	def loop_once(self):
 		cmd_vel = Twist()
 		rect_arrived = rospy.Time.now() - self.rect.header.stamp
 
 		if rect_arrived.to_sec() < 1.0:
 
-			if self.rect.rect.width == 0 and self.rect.rect.height == 0:
-				cmd_vel.linear.x = - 1
-				cmd_vel.linear.y = 1
-				cmd_vel.angular.z = 0.5
-
-			else if self.rect.rect.center.x < 320:
+			if self.rect.rect.center.x < 320:
 				cmd_vel.angular.z = 0.1
+				cmd_vel.linear.x = 0
+				cmd_vel.linear.y = 0
 
 			else:
 				cmd_vel.angular.z = -0.1
+				cmd_vel.linear.x = 0
+				cmd_vel.linear.y = 0
+
+		else:
+			cmd_vel.linear.x = - 0.5
+			cmd_vel.linear.y = 0.5
+			cmd_vel.angular.z = 0.3
 
 		rospy.loginfo("\t\t\t\t\t\tpublish {}".format(cmd_vel.angular.z))
 		self.pub.publish(cmd_vel)		
@@ -55,6 +59,7 @@ class track_box_to_cmd_vel:
 if __name__ == '__main__':
 	try:
 		obj = track_box_to_cmd_vel()
+		obj.loop()
 
 	except rospy.ROSInterruptException:
 		pass
